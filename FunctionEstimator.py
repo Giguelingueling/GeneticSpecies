6d7ead6d9da6d15bd52e428c77f491eb1d606816
Giguelingueling <ulysse.cote-allard.1@ulaval.ca>
2016-09-30 13:06:49 -0400

    Adapted GP for scikit-learn 0.18

--- FunctionEstimator.py
+++ FunctionEstimator.py
@@ -1,11 +1,12 @@
-from sklearn.gaussian_process import GaussianProcess
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern  # As suggested by Larochelle et al.
 import numpy as np
 import math
 
 class FunctionEstimator(object):
 
     def __init__(self, get_EI=False):
-        self._regressor = GaussianProcess(theta0=1e-2, thetaL=1e-4, thetaU=1e-1, random_start=100)
+        self._regressor = GaussianProcessRegressor(kernel=Matern(nu=2.5))
         self._get_EI = get_EI
 
     def train(self, X, y):
@@ -18,8 +19,9 @@ class FunctionEstimator(object):
                     number_of_try += 1
                     self._regressor.fit(np.array(X), np.array(y))
                 else:
-                    self._regressor = GaussianProcess(theta0=math.pow(10,number_of_try)*1e-2, thetaL=1e-4, thetaU=1e-1,
-                                                      random_start=100)
+                    self._regressor = GaussianProcessRegressor(kernel=Matern(nu=2.5),
+                                                               alpha=(math.pow(10, number_of_try/4.0)),
+                                                               n_restarts_optimizer=4)
 
                     self._regressor.fit(np.array(X), np.array(y))
             except Exception:
@@ -27,26 +29,30 @@ class FunctionEstimator(object):
                 no_error = False
 
     def set_EI_bool(self,EI_bool):
-        self._get_EI= EI_bool
+        self._get_EI = EI_bool
 
-    def get_fitness(self, position):
+    def get_fitness(self, position, best_fitness=0.0):
         if self._get_EI == False:
-            self.get_estimation_fitness_value(position)
+            return self.get_estimation_fitness_value(position)
         else:
-            self.get_expected_improvement(position)
+            return self.get_expected_improvement(position, best_fitness)
 
     def get_estimation_fitness_value(self, position):
-        prediction, variance = self._regressor.predict(np.atleast_2d(position), eval_MSE=True)
-        return prediction[0], variance[0]
+        prediction, std = self._regressor.predict(np.atleast_2d(position), return_std=True)
+        return prediction[0], std[0]
 
-    def get_expected_improvement(self, position):
+    def get_expected_improvement(self, position, best_fitness=0.0):
         #TODO write this function to get the EI
-        self.get_estimation_fitness_value(position)
+        prediction, std = self.get_estimation_fitness_value(position)
+        comparaison_predict_vs_best = (prediction-best_fitness)
+        if(comparaison_predict_vs_best < 0):
+            return comparaison_predict_vs_best/std
+        else:
+            return comparaison_predict_vs_best*std
 
     def update_regressor(self, all_creature_data):
         print "UPDATING REGRESSOR"
-        if(self._type_of_regressor == 'Gaussian'):
-            self._regressor.theta0 = self._regressor.theta_# Given correlation parameter = MLE
+        self._regressor.theta0 = self._regressor.kernel_.theta  # Given correlation parameter = MLE
 
         print len(all_creature_data)
         self.train_regressor(all_creature_data)
