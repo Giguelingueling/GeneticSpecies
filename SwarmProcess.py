ed176d8d41b4e02afc378a649988f2062993a1b3
Giguelingueling <ulysse.cote-allard.1@ulaval.ca>
2016-10-02 23:45:51 -0400

    Setted back EI for exploration

--- SwarmProcess.py
+++ SwarmProcess.py
@@ -122,7 +122,7 @@ class SwarmProcess(object):
     def exploration(self):
         print "EXPLORATION"
         # We want to get EI
-        self._regressor.set_EI_bool(False)
+        self._regressor.set_EI_bool(True)
         # We want to get the curiosity
         self._swarm.set_curiosity(True)
         # Make sure that every creature has been evaluated
@@ -151,8 +151,8 @@ class SwarmProcess(object):
         self._regressor.update_regressor(self._list_real_evaluation_position, self._list_real_evaluation_fitness)
         return 0.0
 
-lower_bound = np.array([-5.0, 0.0])
-upper_bound = np.array([10.0, 15.0])
+lower_bound = np.array([-500.0, -500.0])
+upper_bound = np.array([500.0, 500.0])
 number_of_dimensions = 2
 number_of_real_evaluation = 100
 swarm_size = 100
@@ -160,6 +160,6 @@ number_of_generation_swarm = 100
 swarmProcess = SwarmProcess(lower_bound=lower_bound, upper_bound=upper_bound, number_of_dimensions=number_of_dimensions,
                             number_of_real_evaluation=number_of_real_evaluation, swarm_size=swarm_size,
                             number_of_generation_swarm=number_of_generation_swarm,
-                            fitness_function=FitnessFunction.branin, inertia_factor=0.5, self_confidence=1.5,
+                            fitness_function=FitnessFunction.schwefel_function, inertia_factor=0.5, self_confidence=1.5,
                             swarm_confidence=1.5, sense_of_adventure=1.5)
 swarmProcess.run_swarm_process()
