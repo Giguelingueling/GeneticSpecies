d5782289b7a5de80a6d37661da23dd92412dbe44
Gabriel Dubé <gabriel.dube.2@ulaval.ca>
2017-01-02 13:36:29 -0500

    Optimized schaffer function. Normalized the code a tiny bit.

--- FitnessFunction.py
+++ FitnessFunction.py
@@ -10,8 +10,8 @@ def calculate_fitness(fun_fitness, *args):
 # Between -500 and 500
 def schwefel_function(array_genes):
     # We use the Schwefel function slightly modified so it becomes a minimization problem
-    summation = np.sum(array_genes * np.sin(np.sqrt(np.abs(array_genes))))
-    return (418.98288727243374296449474059045314788818359375*float(len(array_genes))) - summation
+    value = np.sum(array_genes * np.sin(np.sqrt(np.abs(array_genes))))
+    return (418.98288727243374296449474059045314788818359375*float(len(array_genes))) - value
 
 
 # Between -5 and 10 for x
@@ -19,28 +19,22 @@ def schwefel_function(array_genes):
 def branin(x):
     x_0 = x[0]
     x_1 = x[1]
-    y = np.square(x_1 - (5.1/(4.0*np.square(math.pi)))*np.square(x_0) + (5.0/math.pi)*x_0 - 6.0) + \
+    value = np.square(x_1 - (5.1/(4.0*np.square(math.pi)))*np.square(x_0) + (5.0/math.pi)*x_0 - 6.0) + \
         10.0*(1-(1./(8*math.pi)))*np.cos(x_0) + 10.0
-
-    result = y
-
-    return result
+    return value
 
 
 # Between -100 and 100
 def schaffer_function(array_genes):
-    summation = 0.0
-    for i in range(len(array_genes)-1):
-        summation += math.pow(math.pow(array_genes[i], 2) + math.pow(array_genes[i + 1], 2), 0.25) * \
-                     (math.pow(math.sin(50 * math.pow(math.pow(array_genes[i], 2) +
-                                                      math.pow(array_genes[i + 1], 2), 0.10)), 2) + 1.0)
-    return summation
+    array = np.square(array_genes[:-1]) + np.square(array_genes[1:])
+    value = np.dot(np.power(array, 0.25), np.square(np.sin(50 * np.power(array, 0.1))) + 1)
+    return value
 
 
 # Between -5.12 and 5.12
 def rastrigin(array_genes):
-    summation = np.dot(array_genes, array_genes) - 10 * np.sum(np.cos(2 * math.pi * array_genes))
-    return summation + 10*len(array_genes)
+    value = np.dot(array_genes, array_genes) - 10 * np.sum(np.cos(2 * math.pi * array_genes))
+    return value + 10*len(array_genes)
 
 
 # Between -1.28 and 1.28
@@ -58,7 +52,7 @@ def schwefel_func_p1_dot_2_unimodal(array_genes):
 
 # Between -10 and 10
 def rosenbrock_(array_genes):
-    array_one = np.power(array_genes[:-1], 2) - array_genes[1:]
+    array_one = np.square(array_genes[:-1]) - array_genes[1:]
     array_two = array_genes[:-1] - 1
     value = 100 * np.dot(array_one, array_one) + np.dot(array_two, array_two)
     return value
