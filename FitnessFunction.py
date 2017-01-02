0ed4d8ae48abfb0d452554b47753faa4b838ba86
Gabriel Dubé <gabriel.dube.2@ulaval.ca>
2017-01-02 14:00:21 -0500

    Added ackley function

--- FitnessFunction.py
+++ FitnessFunction.py
@@ -57,12 +57,21 @@ def rosenbrock(array_genes):
     value = 100 * np.dot(array_one, array_one) + np.dot(array_two, array_two)
     return value
 
+
 # Between -100 and 100
 def elliptic_function(array_genes):
     ndim = len(array_genes)
     value = np.dot(np.power(1000000, np.arange(ndim) / (ndim - 1)), np.square(array_genes))
     return value
 
+
 def sphere_function(array_genes):
     value = np.dot(array_genes, array_genes)
+    return value
+
+# Between -32 and 32
+def ackley_function(array_genes):
+    ndim = len(array_genes)
+    value = 20 - 20 * math.exp(-0.2 * math.sqrt(1.0/ndim * np.dot(array_genes, array_genes)))
+    value += math.e - math.exp(1.0/ndim * np.sum(np.cos(2 * math.pi * array_genes)))
     return value
